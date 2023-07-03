95b608704523744bed8e7f104be43c9d2338c3bd
joao gabriel <jg.7651@gmail.com>
2023-07-03 14:58:28 -0300

    url apenas com protocolo

--- src/components/PreCadastroFormCard.js
+++ src/components/PreCadastroFormCard.js
@@ -100,8 +100,8 @@ const PreCadastroFormCard = ({ ipagId }) => {
             }else{
                 values = { ...values, cpf_cnpj: cnpj, ipagId }
             }
-            let redirectLink = "https://www." + 'festum-site.vercel.app' + "/email-confirmado"
-            //?values=" + JSON.stringify(values)
+            let redirectLink = "https://www." + 'festum-site.vercel.app' 
+            //+ "/email-confirmado?values=" + JSON.stringify(values)
             console.log(redirectLink)
             const result = await efetuarPreCadastroSite(values, redirectLink)
             console.log('Result: ', result);
