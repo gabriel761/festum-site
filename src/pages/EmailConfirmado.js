78af62be456e8adece171b54f0c103bb655225ac
joao gabriel <jg.7651@gmail.com>
2023-07-04 09:16:10 -0300

    teste de pagamento com console log do resultado

--- src/pages/EmailConfirmado.js
+++ src/pages/EmailConfirmado.js
@@ -37,11 +37,12 @@ export default function EmailConfirmado() {
         const clienteIpag = resultClienteIpag?.data
         if(clienteIpag){
             const criarAssinatura = {isActive: true, profile_id: values.email + values.ipagId, profile_id: values.ipagId, customerId: clienteIpag.id, starting_date: formatedDateToday(), callback_url: "https://festum-site.vercel.app/pagamento-confirmado" }
-            const resultPagamentoIpag = await apiIpag.request({
+            const resultAssinaturaIpag= await apiIpag.request({
                 method:'POST',
                 url: '/service/resources/subscriptions',
                 data: criarAssinatura
             })
+            console.log("restult assinatura ipag: ", resultAssinaturaIpag.data)
         }
     }
 
