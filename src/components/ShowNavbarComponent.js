8b64c508909549872b035cfe079044f9c95b274a
joao gabriel <jg.7651@gmail.com>
2023-07-10 18:27:05 -0300

    esconder barra de navegação de algumas paginas

--- src/components/ShowNavbarComponent.js
+++ src/components/ShowNavbarComponent.js
@@ -17,7 +17,7 @@ const ShowNavbarComponent = ({ children, signed }) => {
         }
 
         console.log("this is location: ", location)
-        if (location.pathname == "/" || location.pathname == "/form-precadastro" || location.pathname == "/confirmacao-precadastro" || location.pathname == "email-confirmado" || !signed) {
+        if (location.pathname == "/" || location.pathname == "/form-precadastro" || location.pathname == "/confirmacao-precadastro" || location.pathname == "/email-confirmado" || location.pathname == "/pagamento-confirmado" || !signed) {
             setShowNavbar(false)
         } else {
             setShowNavbar(true)
