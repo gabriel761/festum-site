a5aea61f6565541837d4508936bca7055a208d78
joao gabriel <jg.7651@gmail.com>
2023-10-26 11:19:55 -0300

    testando deep linking usando 2 redirecionamentos seguidos sem async

--- src/pages/RedirectToApp.js
+++ src/pages/RedirectToApp.js
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 
 const RedirectToApp = () => {
-   window.location.replace('festum://')
+    window.location.replace('festum://')
+    window.location.replace('https://play.google.com/store/apps/details?id=com.gabriel761.frontend')
     return null;
 }
  
