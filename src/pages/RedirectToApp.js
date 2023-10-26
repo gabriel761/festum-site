8695ceb2a76aba981a7b7ceea23d5d7587b479fe
joao gabriel <jg.7651@gmail.com>
2023-10-26 11:37:27 -0300

    testando deep link com set timout mas tambem com date

--- src/pages/RedirectToApp.js
+++ src/pages/RedirectToApp.js
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
 
 const RedirectToApp = () => {
-    setTimeout(function () { window.location = "https://play.google.com/store/apps/details?id=com.gabriel761.frontend"; }, 25);
+    var now = new Date().valueOf();
+    setTimeout(function () {
+        if (new Date().valueOf() - now > 100) return;
+        window.location = "https://play.google.com/store/apps/details?id=com.gabriel761.frontend";
+    }, 25);
     window.location = "festum://";
     return null;
 }
