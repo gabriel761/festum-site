6f8d538908b9b4d9cc18ececdab8d30b2c38c690
joao gabriel <jg.7651@gmail.com>
2023-10-26 11:58:21 -0300

    corrigndo erro de logica na condicional

--- src/pages/RedirectToApp.js
+++ src/pages/RedirectToApp.js
@@ -4,9 +4,11 @@ const RedirectToApp = () => {
     var now = new Date().valueOf();
     let notInstalled = false
     setTimeout(function () {
-        if (notInstalled) return;
-        window.location = "https://play.google.com/store/apps/details?id=com.gabriel761.frontend";
-    }, 25);
+        if (notInstalled){
+            window.location = "https://play.google.com/store/apps/details?id=com.gabriel761.frontend";
+        };
+        
+    }, 500);
     window.location = "festum://";
     window.confirm = () => {
         notInstalled = true
