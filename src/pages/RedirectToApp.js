0a278681e32899e223a6dc96fcd5aadc0dd86e0a
joao gabriel <jg.7651@gmail.com>
2023-10-26 11:54:06 -0300

    chegando se app foi instalado

--- src/pages/RedirectToApp.js
+++ src/pages/RedirectToApp.js
@@ -2,11 +2,15 @@ import { useEffect } from "react";
 
 const RedirectToApp = () => {
     var now = new Date().valueOf();
+    let notInstalled = false
     setTimeout(function () {
-        if (new Date().valueOf() - now > 100) return;
+        if (notInstalled) return;
         window.location = "https://play.google.com/store/apps/details?id=com.gabriel761.frontend";
     }, 25);
     window.location = "festum://";
+    window.confirm = () => {
+        notInstalled = true
+    }
     return null;
 }
  
