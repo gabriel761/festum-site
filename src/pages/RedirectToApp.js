c7b9025847c288f8851d113007467e44b1c203a9
joao gabriel <jg.7651@gmail.com>
2023-10-26 12:17:10 -0300

    corrigindo erro no useEffect

--- src/pages/RedirectToApp.js
+++ src/pages/RedirectToApp.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 const RedirectToApp = () => {
     useEffect(() => {
         window.location = "https://play.google.com/store/apps/details?id=com.gabriel761.frontend";
-    })
+    },[])
     
     window.location = "festum://";
     
