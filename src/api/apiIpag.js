f82bb1425d1deb39f21d90e4af8c735b69657210
joao gabriel <jg.7651@gmail.com>
2023-11-14 18:15:24 -0300

    ativar sandbox

--- src/api/apiIpag.js
+++ src/api/apiIpag.js
@@ -1,10 +1,23 @@
 import axios from "axios"
+// export const apiIpag = axios.create({
+//     baseURL: 'https://api.ipag.com.br',
+//     timeout: 3000,
+//     auth: {
+//         username: "festumbrasil@gmail.com",
+//         password: "F043-B605F28B-77B89EF6-91CDC155-6012"
+//     },
+//     headers: {
+//         "Content-Type": "application/json",
+//         "x-api-version": 2
+//     }
+// })
+
 export const apiIpag = axios.create({
-    baseURL: 'https://api.ipag.com.br',
+    baseURL: 'https://sandbox.ipag.com.br',
     timeout: 3000,
     auth: {
-        username: "festumbrasil@gmail.com",
-        password: "F043-B605F28B-77B89EF6-91CDC155-6012"
+        username: "jg.7651@gmail.com",
+        password: "426B-10A599EA-0BD38435-FF3843BD-05BE"
     },
     headers: {
         "Content-Type": "application/json",
